730ed6e4f1b6c851538a77d9212f7884597e31bf
willherzog <willherzog@gmail.com>
2023-11-28 13:40:27 -0700

    Update ItemOptionDefinition.php
    Add option "persist_default" to control whether a non-empty default value (e.g. an enum case) should be persisted (this defaults to FALSE, so is potentially BC-breaking)

--- src/Config/ItemOptionDefinition.php
+++ src/Config/ItemOptionDefinition.php
@@ -19,50 +19,56 @@ class ItemOptionDefinition
 	{
 		$resolver = new OptionsResolver();
 
+		$resolver->define('multiple')
+			->allowedTypes('bool')
+			->default(false)
+			->info('Controls whether array values are stored as multiple database rows (arrays are always serialized otherwise).')
+		;
+
 		$resolver->define('default')
 			->default(null)
 			->info('Fallback value for when this option has not been persisted (this will always be an empty array if "multiple" is TRUE).')
 		;
 
-		$resolver->define('multiple')
-			->default(false)
+		$resolver->define('persist_default')
 			->allowedTypes('bool')
-			->info('Controls whether array values are stored as multiple database rows (arrays are always serialized otherwise).')
+			->default(false)
+			->info('Whether to persist this option when the value matches the default/fallback value. Not applicable if the value is already covered by one of the allow_* options.')
 		;
 
 		$resolver->define('allow_empty_array')
-			->default(false)
 			->allowedTypes('bool')
+			->default(false)
 			->info('Whether to persist this option when the value is an empty array.')
 		;
 
 		$resolver->define('allow_empty_string')
-			->default(false)
 			->allowedTypes('bool')
+			->default(false)
 			->info('Whether to persist this option when the value is an empty string.')
 		;
 
 		$resolver->define('allow_zero')
-			->default(false)
 			->allowedTypes('bool')
+			->default(false)
 			->info('Whether to persist this option when the value is the number zero (either an integer or a float).')
 		;
 
 		$resolver->define('allow_false')
-			->default(false)
 			->allowedTypes('bool')
+			->default(false)
 			->info('Whether to persist this option when the value is FALSE.')
 		;
 
 		$resolver->define('allow_null')
-			->default(false)
 			->allowedTypes('bool')
+			->default(false)
 			->info('Whether to persist this option when the value is NULL.')
 		;
 
 		$resolver->define('requirement_callback')
+			->allowedTypes('callable', 'null')
 			->default(null)
-			->allowedTypes('null', 'callable')
 			->info('Boolean-returning function to determine whether given host item instance fulfills any non-static requirements for having this option.')
 		;
 
@@ -85,7 +91,7 @@ class ItemOptionDefinition
 
 	public function getDefaultValue(): mixed
 	{
-		return $this->persistWithMultipleRows() ? [] : $this->config['default'];
+		return $this->config['multiple'] ? [] : $this->config['default'];
 	}
 
 	public function shouldPersistValue(mixed $value): bool
@@ -96,7 +102,15 @@ class ItemOptionDefinition
 			0, 0.0 => $this->config['allow_zero'],
 			false => $this->config['allow_false'],
 			null => $this->config['allow_null'],
-			default => true
+			default => function () use ($value): bool {
+				$defaultValue = $this->getDefaultValue();
+
+				if( !empty($defaultValue) && $value === $defaultValue ) {
+					return $this->config['persist_default'];
+				}
+
+				return true;
+			}
 		};
 	}
 }
